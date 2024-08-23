7a5dcc1c044d81738e2078d9edb4ff6a272e3004
johan-akn <johan.akn17@gmail.com>
2024-08-23 17:16:44 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Dinossauros</h1>
+    <h2>Carnívoros</h2>
+    <p>Mussum Ipsum, cacilds vidis litro abertis.  Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Casamentiss faiz malandris se pirulitá. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.</p>
+    </>
+  )
+}
+
+export default App
