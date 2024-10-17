45bc9789c39c905841876e47c12f61c10d5ebf2d
johan-akn <johan.akn17@gmail.com>
2024-10-17 19:54:15 -0300

    save

--- exercicios_rotas/src/pages/Pares.jsx
+++ exercicios_rotas/src/pages/Pares.jsx
@@ -4,9 +4,30 @@ import Navbar from '../components/Navbar'
 import './Pares.css'
 
 function Pares() {
-    const{resultado, setResultado} = useState(false)
-    const{inptNumeroUm, setInptNumeroUm} = useState('')
-    const{inptNumeroDois, setInptNumeroDois} = useState('')
+    const[resultado, setResultado] = useState(false)
+    const[inptNumeroUm, setInptNumeroUm] = useState('')
+    const[inptNumeroDois, setInptNumeroDois] = useState('')
+    const[cont, setCont] = useState(0)
+
+    function LerPares(){
+      let maiorNum = 0, menorNum = 0
+      let numUm = Number(inptNumeroUm), numDois = Number(inptNumeroDois)
+      let numeroSobra
+
+      if(numUm > maiorNum){
+        maiorNum = numUm
+      }else if(numDois > maiorNum){
+        maiorNum = numDois
+      }
+
+      if(numUm < menorNum){
+        menorNum = numUm
+      }else if(numDois < menorNum){
+        menorNum = numDois
+      }
+
+      numeroSobra = maiorNum - menorNum
+    }
 
   return (
     <div>
@@ -14,15 +35,21 @@ function Pares() {
         <div className="container-pares">
             <h1>7.13 e 7.14</h1>
 
-            <input type="number"
-            value={inptNumeroUm}
-            onChange={ (e) => setInptNumeroUm(e.target.value) } />
+            <div className="container-inpt">
+              <p>Número 1</p>
+              <input type="number"
+              value={inptNumeroUm}
+              onChange={ (e) => setInptNumeroUm(e.target.value) } />
+            </div>
             
-            <input type="number"
-            value={inptNumeroDois}
-            onChange={ (e) => setInptNumeroDois(e.target.value) } />
+            <div className="container-inpt">
+              <p>Número 2</p>
+              <input type="number"
+              value={inptNumeroDois}
+              onChange={ (e) => setInptNumeroDois(e.target.value) } />
+            </div>
 
-            <button>Ler</button>
+            <button onClick={LerPares}>Ler</button>
             <button onClick={ () => setResultado(true) }>RESULTADO</button>
 
             {resultado && <p>z</p>}
